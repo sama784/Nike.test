c893dd0a6ec39d55511e4a507eed5524997bc3b6
sama784 <samamohammed784@gmail.com>
2026-03-16 10:19:59 +0200

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,27 @@
-function changeMainColor(colorName){
+function changeMainColor(colorName) {
     let html = document.querySelector("html"),
         newColor = getComputedStyle(html).getPropertyValue(`--${colorName}`);
 
-        html.style.setProperty("--main-color", newColor);
+        changeNavImg(colorName.split("-")[0]);
+        changeIcon(colorName.split("-")[0]);
+    html.style.setProperty("--main-color", newColor);
+}
+
+function changeNavImg(imgName){
+    let currentImgSrc = Logo.src,
+        currentImgSrcArr = currentImgSrc.split("/");
+
+    currentImgSrcArr[currentImgSrcArr.length - 1] = `${imgName}-logo.png`;
+
+    Logo.src =currentImgSrcArr.join("/");
+}
+
+function changeIcon(imgName){
+    let icon = document.getElementById("Icon"),
+        currentIcon = icon.getAttribute("href"),
+        currentIconArr = currentIcon.split("/");
+
+    currentIconArr[1] = `${imgName}-logo.png`;
+
+    icon.setAttribute("href", currentIconArr.join("/"));
 }
--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 let carousel = document.querySelector('#SC-carousel'),
     nextSliderBtn = carousel.querySelector('button.next'),
     prevSliderBtn = carousel.querySelector('button.prev'),
-    carouselSlide = carousel.querySelectorAll('.carousel-item');
+    carouselSlide = carousel.querySelectorAll('.carousel-item'),
+    navEle = document.querySelector("nav"),
+    heightOfNav = navEle.clientHeight;
 
 nextSliderBtn.addEventListener("click", function () {
     let currentSlide = carousel.querySelector('.carousel-item.active'),
-        nextSlide = currentSlide.nextElementSibling ?? carousel.querySelector(".carousel-item:first-child");
+        nextSlide = currentSlide.nextElementSibling ?? carousel.querySelector(".carousel-item:first-child"),
+        currentColorName = nextSlide.dataset.colorName;
 
-     currentSlide.classList.remove("active", "show");
+    changeMainColor(currentColorName);
+
+    currentSlide.classList.remove("active", "show");
     nextSlide.classList.add("active");
 
     setTimeout(() => {
@@ -16,9 +21,12 @@ nextSliderBtn.addEventListener("click", function () {
 });
 prevSliderBtn.addEventListener("click", function () {
     let currentSlide = carousel.querySelector('.carousel-item.active'),
-        prevSlide = currentSlide.previousElementSibling ?? carousel.querySelector(".carousel-item:last-child");
+        prevSlide = currentSlide.previousElementSibling ?? carousel.querySelector(".carousel-item:last-child"),
+        currentColorName = prevSlide.dataset.colorName;
 
-     currentSlide.classList.remove("active", "show");
+    changeMainColor(currentColorName);
+
+    currentSlide.classList.remove("active", "show");
     prevSlide.classList.add("active");
 
     setTimeout(() => {
@@ -26,12 +34,20 @@ prevSliderBtn.addEventListener("click", function () {
     }, 100);
 });
 
-window.addEventListener("DOMContentLoaded" , function (){
+window.addEventListener("DOMContentLoaded", function () {
     carouselSlide[0].classList.add("active");
-    
-    setTimeout(function(e){
+
+    setTimeout(function (e) {
         carouselSlide[0].classList.add("show");
 
-    },100)
+    }, 100)
 
-})
+});
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 5) {
+        navEle.classList.add("scrolled");
+    } else {
+        navEle.classList.remove("scrolled");
+    }
+});
